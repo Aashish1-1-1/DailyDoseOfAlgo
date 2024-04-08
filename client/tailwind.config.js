6607f5b89d9fc5ec100728635f156af90bda6a2a
Sanjib Dahal <sanjibdahal04@gmail.com>
2024-04-08 23:54:16 +0545

    created some frontend designs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+    },
     extend: {
       fontFamily: {
         "poppins": ['Poppins', 'sans-serif'],
