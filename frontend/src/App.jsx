7f558330b5d8f101a575824beafaab325ba74da8
Sanjib Dahal <sanjibdahal04@gmail.com>
2024-04-21 23:47:10 +0545

    created some ui in landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,41 +35,75 @@ const App = () => {
           <h1 className="font-black font-playfair text-white text-6xl text-center leading-snug max-md:text-5xl max-sm:text-4xl">Empowering developers to enhance their algorithmic skills through a daily regimen of challenges and content.</h1>
         </section>
 
-        <section className="bg-black min-h-[100vh] w-full flex flex-col px-10 max-md:px-6 max-sm:px-6 justify-center">
+        <section className="bg-black py-16 w-full flex flex-col px-20 max-md:px-6 max-sm:px-6 justify-center">
           <h2 className="font-bold font-playfair text-white text-5xl leading-normal">Algorithm Mastery Awaits</h2>
-          <p className="font-poppins text-white">Embark on your journey to expertise.</p>
+          <p className="font-poppins text-white font-light">Embark on your journey to expertise.</p>
 
-          <div className="cards flex gap-3 flex-wrap justify-center">
-            <div className="card border-2 w-[300px] rounded-xl">
-              <img src="./assets/image1.png" alt="" className="w-[300px] bg-cover overflow-hidden rounded-t-xl" />
+          <div className="cards flex gap-8 flex-wrap mt-8">
+            <div className="card border-2 w-[350px] rounded-xl">
+              <img src="./assets/image1.png" alt="" className="w-[350px] bg-cover overflow-hidden rounded-t-xl" />
               <div className="content p-4">
-                <p className="text-white">Sharpen your skills</p>
-                <h4 className="text-white">Daily Custom Challenges</h4>
-                <h6 className="text-white">Tailored problems to fit your skill level.</h6>
-                <a href="" className="text-white">Accept Challenge</a>
+                <p className="text-white font-extralight text-xs mb-1">Sharpen your skills</p>
+                <h4 className="text-white font-medium text-2xl">Daily Custom Challenges</h4>
+                <h6 className="text-white font-extralight text-sm mb-2">Tailored problems to fit your skill level.</h6>
+                <a href="" className="text-white font-light text-xs">Accept Challenge</a>
               </div>
             </div>
 
-            <div className="card border-2 w-[300px] rounded-xl">
-              <img src="./assets/image2.png" alt="" className="w-[300px] bg-cover overflow-hidden rounded-t-xl" />
+            <div className="card border-2 w-[350px] rounded-xl">
+              <img src="./assets/image2.png" alt="" className="w-[350px] bg-cover overflow-hidden rounded-t-xl" />
               <div className="content p-4">
-                <p className="text-white">Sharpen your skills</p>
-                <h4 className="text-white">Daily Custom Challenges</h4>
-                <h6 className="text-white">Tailored problems to fit your skill level.</h6>
-                <a href="" className="text-white">Accept Challenge</a>
+                <p className="text-white font-extralight text-xs mb-1">Interactive Learning</p>
+                <h4 className="text-white font-medium text-2xl">Real-time Feedback System</h4>
+                <h6 className="text-white font-extralight text-sm mb-2">Instant evaluation to guide your progress.</h6>
+                <a href="" className="text-white font-light text-xs">Try Now</a>
               </div>
             </div>
 
-            <div className="card border-2 w-[300px] rounded-xl">
-              <img src="./assets/image3.png" alt="" className="w-[300px] bg-cover overflow-hidden rounded-t-xl" />
+            <div className="card border-2 w-[350px] rounded-xl">
+              <img src="./assets/image3.png" alt="" className="w-[350px] bg-cover overflow-hidden rounded-t-xl" />
               <div className="content p-4">
-                <p className="text-white">Sharpen your skills</p>
-                <h4 className="text-white">Daily Custom Challenges</h4>
-                <h6 className="text-white">Tailored problems to fit your skill level.</h6>
-                <a href="" className="text-white">Accept Challenge</a>
+                <p className="text-white font-extralight text-xs mb-1">Community Wisdom</p>
+                <h4 className="text-white font-medium text-2xl">Engage with Fellow Coders</h4>
+                <h6 className="text-white font-extralight text-sm mb-2">Share insights and gain new perspectives.</h6>
+                <a href="" className="text-white font-light text-xs">Join Discussion</a>
               </div>
             </div>
           </div>
+        </section>
+
+        <section className="bg-slate-950 min-h-[100vh] w-full flex flex-col max-md:px-6 max-sm:px-6 px-20 py-20">
+        <h2 className="font-bold font-playfair text-white text-5xl leading-normal">Getting Started</h2>
+
+        <div className="content flex border-b-2 border-gray-500 mb-10 pb-6">
+          <h3 className="font-bold font-playfair text-white text-9xl">1</h3>
+          <div className='flex flex-col justify-center items-center w-full'>
+            <div>
+              <h3 className='text-3xl font-normal mb-2'>Sign Up</h3>
+              <p className="text-white font-light">Create an account to access the full range of features.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="content flex border-b-2 border-gray-500 mb-10 pb-6">
+          <h3 className="font-bold font-playfair text-white text-9xl">2</h3>
+          <div className='flex flex-col justify-center items-center w-full'>
+            <div>
+              <h3 className='text-3xl font-normal mb-2'>Choose a Lesson</h3>
+              <p className="text-white font-light">Select topic from our extensive library of algorithms.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="content flex border-b-2 border-gray-500 mb-10 pb-6">
+          <h3 className="font-bold font-playfair text-white text-9xl">3</h3>
+          <div className='flex flex-col justify-center items-center w-full'>
+            <div>
+              <h3 className='text-3xl font-normal mb-2'>Solve the Quiz</h3>
+              <p className="text-white font-light">Implement your learning and solve the quizzes.</p>
+            </div>
+          </div>
+        </div>
 
 
         </section>
