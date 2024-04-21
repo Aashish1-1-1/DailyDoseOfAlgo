beee4f0dfbc8cac43506d4e82e0931a72492b89c
Sanjib Dahal <sanjibdahal04@gmail.com>
2024-04-21 22:11:16 +0545

    tailwind css setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h1 className='text'>Hello, Tailwind!</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
