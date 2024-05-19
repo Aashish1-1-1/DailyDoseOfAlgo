81e2675afebc83abdae82439acd51efee1990430
Sanjib Dahal <135316801+sanjibdahal@users.noreply.github.com>
2024-05-19 17:40:57 +0545

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/sanjibdahal"
+                    href="https://github.com/sanjibdahal/DailyDoseOfAlgo"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
