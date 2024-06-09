1e179f08127357cd35617edcd3185d2b7f31000d
Sanjib Dahal <sanjibdahal04@gmail.com>
2024-06-10 00:11:41 +0545

    feat: google oauth but need to be fix

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,53 +1,20 @@
-// import React from "react";
-// import Header from "./components/Header/Header";
-// import { Outlet, redirect } from "react-router-dom";
-// import Footer from "./components/Footer/Footer";
-// import { AuthProvider, useAuth } from "./Context/Auth";
-
-// const Layout = () => {
-
-//   const auth = useAuth();
-//   let isAuthenticated = auth.isAuthenticated;
-//   console.log(auth)
-//   // isAuthenticated = auth.isAuthenticated;
-//   // let isAuthenticated = false;
-
-//   const PublicRoutes = ["/", "/login", "/signup", "/contact", "/about", "/termsandconditions"];
-
-//   if (!isAuthenticated && !PublicRoutes.includes(window.location.pathname)) {
-//     return redirect("/login");
-//   }
-
-
-//   return (
-//     <>
-//       <AuthProvider>
-//         <Header />
-//         <Outlet />
-//         <Footer />
-//       </AuthProvider>
-//     </>
-//   );
-// };
-
-// export default Layout;
-
-import React from "react";
-import Header from "./components/Header/Header";
-import { Outlet, useNavigate, redirect } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import { AuthProvider, useAuth } from "./Context/Auth";
+import React from 'react';
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { AuthProvider, useAuth } from './Context/Auth';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-
 const Layout = () => {
-  const PublicRoutes = ["/", "/login", "/signup", "/contact", "/about", "/termsandconditions", "/auth/success"];
+  const PublicRoutes = ['/', '/login', '/signup', '/contact', '/about', '/termsandconditions', '/auth/success'];
 
   return (
     <AuthProvider>
-      <Header />
-      <LayoutContent PublicRoutes={PublicRoutes} />
-      <Footer />
+      <GoogleOAuthProvider clientId='903882814714-9uqs45mhrs8kn4evvrq581iqdm8pfm96.apps.googleusercontent.com'>
+        <Header />
+        <LayoutContent PublicRoutes={PublicRoutes} />
+        <Footer />
+      </GoogleOAuthProvider>
     </AuthProvider>
   );
 };
@@ -67,16 +34,7 @@ const LayoutContent = ({ PublicRoutes }) => {
     return <Navigate to="/login" replace />;
   }
 
-  return (
-    <>
-      <GoogleOAuthProvider clientId="903882814714-9uqs45mhrs8kn4evvrq581iqdm8pfm96.apps.googleusercontent.com" >
-        <Header />
-        <Outlet />
-        <Footer />
-      </GoogleOAuthProvider>
-    </>
-  );
+  return <Outlet />;
 };
 
 export default Layout;
-
